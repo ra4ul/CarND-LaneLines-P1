4c54f78fb0a3379e13bc4b6f8b8a3629892474d9
Rahul Abrol <rabrol@naturalinsight.com>
2017-03-01 14:35:43 -0500

    Finalized pipeline, saved overlay images.

--- pipeline.py
+++ pipeline.py
@@ -1,33 +1,38 @@
-# convert to grayscale
-gray = grayscale(image)
-#plt.imshow(gray, cmap='gray')
+def pipeline(image):
+    # convert to grayscale
+    gray = grayscale(image)
 
-# gaussian smoothing
-kernel_size = 5 # parameter
-blur_gray = gaussian_blur(gray, kernel_size)
+    # gaussian smoothing
+    kernel_size = 3 # parameter
+    blur_gray = gaussian_blur(gray, kernel_size)
 
-# canny edge detection
-low_threshold = 50 # parameter
-high_threshold = 150 # parameter
-edges = canny(blur_gray, low_threshold, high_threshold)
+    # canny edge detection
+    low_threshold = 60 # parameter
+    high_threshold = 180 # parameter
+    edges = canny(blur_gray, low_threshold, high_threshold)
 
-# mask region
-vertices = np.array([[(60,image.shape[0]),(420, 300), (image.shape[1] - 420, 300), (image.shape[1] - 60,image.shape[0])]], dtype=np.int32) # parameter
-masked_image = region_of_interest(edges, vertices)
+    # mask region
+    vertices = np.array([[(110, image.shape[0]),
+                          (450, 330),
+                          (520, 330),
+                          (image.shape[1] - 60, image.shape[0])]],
+                        dtype=np.int32) # parameter
+    masked_image = region_of_interest(edges, vertices)
 
-# hough transform
-rho = 1 # parameter
-theta = math.pi / 90 # parameter
-threshold = 30 # parameter
-min_line_len = 10 # parameter
-max_line_gap = 1 # parameter
-hough = hough_lines(masked_image, rho, theta, threshold, min_line_len, max_line_gap)
+    # hough transform
+    rho = 1 # parameter
+    theta = math.pi / 90 # parameter
+    threshold = 10 # parameter
+    min_line_len = 5 # parameter
+    max_line_gap = 10 # parameter
+    hough = hough_lines(masked_image, rho, theta, threshold, min_line_len, max_line_gap)
 
-# overlay
-overlay = weighted_img(hough, image)
+    # return overlay
+    return weighted_img(hough, image)
 
-# final result
-plt.imshow(overlay)
+for im in os.listdir("test_images/"):
+    # read image
+    image = mpimg.imread('test_images/' + im)
 
-# saved
-mpimg.imsave('test_images/solidWhiteRight_overlay.jpg', overlay)
+    # save result
+    mpimg.imsave("test_images/overlay_" + im, pipeline(image))
